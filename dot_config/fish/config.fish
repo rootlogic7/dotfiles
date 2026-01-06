bf5bb61e6c0b1aeadecbd16d9693691aeb1a46c4
haku <rootlogic7@proton.me>
2026-01-06 13:41:50 +0100

    Default editor zu neovim geändert, Abbreviations für chezmoi und git workflow hinzugefügt

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,24 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-#test
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
+
+# Chezmoi Abkürzungen
+abbr --add cm 'chezmoi'
+abbr --add cma 'chezmoi apply'
+abbr --add cme 'chezmoi edit'
+abbr --add cmd 'chezmoi diff'  # Zeigt dir, was sich geändert hat, bevor du apply machst
+abbr --add cmu 'chezmoi update' # Holt Änderungen von Git und wendet sie an (pull + apply)
+
+# Git Workflow für Chezmoi (optional, geht auch manuell)
+function cmcp
+    chezmoi git add .
+    chezmoi git -- commit -m "$argv"
+    chezmoi git push
+end
