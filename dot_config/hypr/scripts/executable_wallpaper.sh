626a27eec28389eb84777a2df185e47d27c99860
haku <rootlogic7@proton.me>
2026-01-15 12:11:06 +0100

    changing wallpaper

--- dot_config/hypr/scripts/executable_wallpaper.sh
+++ dot_config/hypr/scripts/executable_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pfad zu deinem statischen Bild (JPG/PNG bevorzugt für Performance)
-WALLPAPER="$HOME/.local/share/backgrounds/3440x1440__00012_.png"
+WALLPAPER="$HOME/.local/share/backgrounds/3440x1440__00014_.png"
 
 # 1. Daemon starten, falls er nicht läuft
 if ! pgrep -x "swww-daemon" > /dev/null; then
